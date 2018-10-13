5454535be6c0680a0329af29de4f13fe698e5e19
ozelot <contact@ozelot.jp>
2018-10-13 10:52:01 +0900

    Update app.user.js

--- app.user.js
+++ app.user.js
@@ -2,49 +2,65 @@
 // @name ozelotjp/dotcom-js
 // @homepageURL https://github.com/ozelotjp/dotcom-js
 // @match https://www.itpassportsiken.com/ipkakomon.php
+// @match https://www.itpassportsiken.com/kakomon/*/*.html
 // @match https://www.sg-siken.com/sgkakomon.php
+// @match https://www.sg-siken.com/kakomon/*/*.html
 // @match https://www.fe-siken.com/fekakomon.php
+// @match https://www.fe-siken.com/kakomon/*/*.html
 // @match https://www.ap-siken.com/apkakomon.php
+// @match https://www.ap-siken.com/kakomon/*/*.html
 // @match https://www.sc-siken.com/sckakomon.php
+// @match https://www.sc-siken.com/kakomon/*/*.html
 // @match https://www.nw-siken.com/nwkakomon.php
+// @match https://www.nw-siken.com/kakomon/*/*.html
 // @match https://www.db-siken.com/dbkakomon.php
+// @match https://www.db-siken.com/kakomon/*/*.html
 // @match https://www.pm-siken.com/pmkakomon.php
+// @match https://www.pm-siken.com/kakomon/*/*.html
 // ==/UserScript==
-
-$(function(){
-  // hidden except main column
+$(function() {
   $("#headerWrap").remove();
   $("#headerMenuWrap").remove();
   $("#sideCol").remove();
   $("#footerWrap").remove();
-  
-  // extend width of main column to 100%
+
   $("#mainCol").css({
-    "display": "block",
-    "width": "100%"
+    display: "block",
+    width: "100%"
   });
 
-  $(window).keydown(function(event){
-    // answer
-    switch(event.key){
-      case "a": $(".selectBtn")[0].click(); break;
-      case "i": $(".selectBtn")[1].click(); break;
-      case "u": $(".selectBtn")[2].click(); break;
-      case "e": $(".selectBtn")[3].click(); break;
-    }
-    switch(event.key){
-      case "a":
-      case "i":
-      case "u":
-      case "e":
-        $("html").stop().animate({
-          scrollTop: $(".answerBox").offset().top -50
-        });
-    }
-    
-    // next page
-    if(event.key === "Enter" && $("#kaisetsu").css("display") === "block"){
-      $(".submit")[0].click();
+  $(window).keydown(function(event) {
+    if (document.URL.match("pm*.html")) {
+      // nothing
+    } else {
+      switch (event.key) {
+        case "a":
+          $(".selectBtn")[0].click();
+          break;
+        case "i":
+          $(".selectBtn")[1].click();
+          break;
+        case "u":
+          $(".selectBtn")[2].click();
+          break;
+        case "e":
+          $(".selectBtn")[3].click();
+          break;
+      }
+      switch (event.key) {
+        case "a":
+        case "i":
+        case "u":
+        case "e":
+          $("html")
+            .stop()
+            .animate({
+              scrollTop: $(".answerBox").offset().top - 50
+            });
+      }
+      if (event.key === "Enter" && $("#kaisetsu").css("display") === "block") {
+        $(".submit")[0].click();
+      }
     }
   });
 });
